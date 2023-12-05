b7a1d3f8c1b5e1d4dff8dd0e43e9f44c07d76bde
mgwr <aaskarov870@gmail.com>
2023-12-06 00:31:13 +0500

    new file:   .vscode/settings.json 	new file:   .vscode/tasks.json 	modified:   03_2_02main.cpp 	modified:   03_2_02task.cpp

---
+++ 03_2_02main.cpp
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "03_2_02task.cpp"
+// #include "os_mem.h"
+
+int main(){
+    memory_manager_t mm;
+    setup_memory_manager(&mm);
+    
+    // mm.create(1000, 1);
+    // mm.alloc(10);
+    // mm.alloc(10);
+    // mm.alloc(10);
+    // mm.alloc(10);
+    return 0;
+}
---
+++ 03_2_02task.cpp
@@ -0,0 +1,118 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "os_mem.h"
+
+typedef struct super_duper_stack{
+    super_duper_stack *parent;
+    super_duper_stack *child;
+    mem_handle_t *mmt;
+} super_duper_stack;
+
+super_duper_stack *__m = NULL;
+long long int __stksz = -1;
+long long int __ocpdsz = -1;
+
+int create(int size, int num_pages);
+int get_free_space();
+mem_handle_t alloc(int block_size);
+int free(mem_handle_t h);
+int destroy();
+
+int check_stack(super_duper_stack *block);
+int init_stack(int size, int num_pages);
+mem_handle_t alloc_block(int block_size);
+int free_block(mem_handle_t h);
+int rm_stack();
+
+void setup_memory_manager(memory_manager_t *mm){
+    mm->create = create;
+    mm->alloc = alloc;
+    mm->free = free;
+    mm->destroy = destroy;
+    mm->get_free_space = get_free_space;
+}
+
+int rm_stack(){
+    while(__m->child != NULL) __m = __m->child;
+    while(__m->parent != NULL){
+        __m = __m->parent;
+        free_block(mem_handle_t(__m->child->mmt->addr, __m->child->mmt->size));
+    }
+    free(__m->mmt);
+    free(__m);
+    __m = NULL;
+    __ocpdsz = -1;
+    __stksz = -1;
+    return 1;
+}
+
+int free_block(mem_handle_t h){
+    while(__m->child != NULL) __m = __m->child;
+    super_duper_stack *rm_block = __m;
+    if(rm_block->mmt->addr != h.addr || rm_block->mmt->size != h.size) return 0;
+    __ocpdsz -= rm_block->mmt->size;
+    __m = __m->parent;
+    free(rm_block->mmt);
+    rm_block->child = NULL;
+    rm_block->parent = NULL;
+    free(rm_block);
+    __m->child = NULL;
+    return 1;
+    // int is_exist = 0;
+    // while(__m->parent != NULL) __m = __m->parent;
+    // while(__m->child != NULL){
+    //     if(__m->mmt->addr == h.addr && __m->mmt->size == h.size){
+    //         is_exist = 1;
+    //         break;
+    //     }
+    //     __m = __m->child;
+    // }
+    // if(!is_exist) return 0;
+    // super_duper_stack *free_block = __m;
+    // free_block->parent->child = free_block->child;
+}
+
+mem_handle_t alloc_block(int block_size){
+    if(block_size <= 0 || block_size > __stksz || __ocpdsz + block_size > __stksz) return mem_handle_t(0,0);
+    while(__m->child != NULL) __m = __m->child;
+    
+    super_duper_stack *new_block = (super_duper_stack*)malloc(sizeof(super_duper_stack));
+    new_block->parent = __m;
+    __m->child = new_block;
+    new_block->child = NULL;
+    
+    new_block->mmt = (mem_handle_t*)malloc(sizeof(mem_handle_t));
+    if(!__m->parent){
+        new_block->mmt->addr = 0;
+        new_block->mmt->size = block_size;
+        printf("allocated < %d | %d >\n", new_block->mmt->addr, new_block->mmt->size);
+    }
+    else{
+        new_block->mmt->addr = __m->mmt->size + __m->mmt->addr;
+        new_block->mmt->size = block_size;
+        printf("allocated < %d | %d >\n", new_block->mmt->addr, new_block->mmt->size);
+    }
+    
+
+    return mem_handle_t(new_block->mmt->addr, new_block->mmt->size);
+}
+
+int init_stack(int size, int num_pages){
+    if(size <= 0) return 0;
+    __m = (super_duper_stack*)malloc(sizeof(super_duper_stack));
+    if(!__m) exit(0);
+    __m->child = NULL;
+    __m->parent = NULL;
+    __m->mmt = NULL;
+    __stksz = size;
+    return 1;
+}
+
+
+int create(int size, int num_pages) { return check_stack(__m) == 0 ? init_stack(size, num_pages) : 0; }
+mem_handle_t alloc(int block_size) { return check_stack(__m) == 0 ? mem_handle_t(0,0) : alloc_block(block_size); }
+int free(mem_handle_t h){ return check_stack(__m) == 0 ? 0 : free_block(h); }
+int destroy(){ return check_stack(__m) == 0 ? 0 : rm_stack(); }
+
+int get_free_space(){ return __stksz - __ocpdsz; }
+int check_stack(super_duper_stack *block) { return block == NULL ? 0 : 1; }
